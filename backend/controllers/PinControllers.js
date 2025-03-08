6b478a06de891b9b65a2474a4376e69915816675
[Waseem] <[waseemkhan3703@gmail.com]>
2025-03-08 23:41:18 +0500

    backend completed

--- backend/controllers/PinControllers.js
+++ backend/controllers/PinControllers.js
@@ -26,3 +26,135 @@ export const CreatePin = TryCatch(async (req, res) => {
 
     res.json({ message: "Pin Created" });
 });
+
+
+export const getAllPins = TryCatch(async(req, res)=>{
+    const pins = await Pin.find().sort({ createdAt : -1 });
+    
+    res.json(pins);
+});
+
+export const getSinglePin = TryCatch(async(req, res)=>{
+    const pin = await Pin.findById(req.params.id).populate("owner", "-password");
+    
+    res.json(pin);
+});
+
+export const commentOnPin = TryCatch(async(req, res)=>{
+    const pin = await Pin.findById(req.params.id);
+    
+    if(!pin){  
+        return res.json({
+        message: "No pin with this id",
+    
+    });
+};
+
+    pin.comments.push({
+        user: req.user._id,
+        name: req.user.name,
+        comment: req.body.comment,
+
+    });
+    
+    await pin.save()
+
+    res.json({
+        message: "Comment Added Sucessfuly",
+    });
+});
+
+
+export const DeleteComment = TryCatch(async(req, res) =>{
+
+    const pin = await Pin.findById(req.params.id);
+
+    if(!pin){
+        return res.status(400).json({
+            message: "No pin with this id",
+        });
+    };
+    if(!req.query.commentId){
+        return res.status(404).json({
+            message: "Please Provide comment id",
+        });
+    };
+
+    const CommentIndex = pin.comments.findIndex(
+        (item) => item._id.toString() == req.query.commentId.toString()
+    );
+    if(!CommentIndex === -1){
+        return res.status(404).json({
+        message: "No Commment with this id",
+    });
+    };
+
+    const comment = pin.comments[CommentIndex];
+    
+    if(comment.user.toString() === req.user._id.toString()){
+        pin.comments.splice(CommentIndex, 1);
+     
+        await pin.save();
+
+        return res.json({
+            message: "Comment Deleted Succesfully",
+        });
+    }
+    else{
+        return res.status(403).json({
+            message: "You are not owner of this comment",
+        });
+    };
+        
+});
+
+
+export const deletePin = TryCatch(async(req, res)=>{
+    const pin = await Pin.findById(req.params.id)
+
+    if (!pin)
+        return res.status(400).json({
+        message: "No pin with this id",
+    
+    });
+
+    if(pin.owner.toString() !== req.user._id.toString())
+        return res.status(403).json({
+    message: "Unauthorized",
+});
+    
+    await cloudinary.v2.uploader.destroy(pin.image.id)
+
+    await pin.deleteOne();
+
+    res.json({
+        message: "Pin Deleted Successfuly",
+    });
+
+
+});
+
+
+export const UpdatePin = TryCatch( async(req, res)=>{
+  const pin = await Pin.findById(req.params.id)
+
+    if (!pin)
+    return res.status(400).json({
+    message: "No pin with this id",
+
+    });
+    if(pin.owner.toString() !== req.user._id.toString())
+        return res.status(403).json({
+    message: "Unauthorized",
+    });
+
+    pin.title = req.body.title;
+    pin.pin = req.body.pin;
+
+    await res.save();
+
+    res.json({
+        message: "Pin Updated Successfuly",
+    })
+
+});
--- backend/routes/PinRoutes.js
+++ backend/routes/PinRoutes.js
@@ -1,11 +1,21 @@
 import express from "express";
 import isAuth from "../middleware/isAuth.js";
 import UploadFile from "../middleware/multer.js";
-import { CreatePin } from "../controllers/PinControllers.js";
+import { 
+    commentOnPin, CreatePin, DeleteComment, deletePin, getAllPins, getSinglePin,
+    UpdatePin
+ } from "../controllers/PinControllers.js";
 
 const router = express.Router();
 
 
-router.post("/new", isAuth, UploadFile, CreatePin)
+router.post("/new", isAuth, UploadFile, CreatePin);
+router.get("/all", isAuth, getAllPins);
+router.get("/:id", isAuth, getSinglePin);
+router.delete("/:id", isAuth, deletePin);
+router.put("/:id", isAuth, UpdatePin);
+router.post("/comment/:id", isAuth, commentOnPin);
+router.delete("/comment/:id", isAuth, DeleteComment);
+
 
 export default router;
