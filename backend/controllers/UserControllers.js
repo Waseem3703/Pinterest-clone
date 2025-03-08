f88b622b96471287604577b6bb84dd19e793738b
[Waseem] <[waseemkhan3703@gmail.com]>
2025-03-08 19:42:00 +0500

    pinterest project

--- backend/controllers/UserControllers.js
+++ backend/controllers/UserControllers.js
@@ -1,134 +1,109 @@
 import { User } from "../models/UserModel.js";
-import bcrypt from   "bcrypt";
+import bcrypt from "bcrypt";
 import TryCatch from "../utils/TryCatch.js";
 import GenerateToken from "../utils/GenerateToken.js";
-
 
 export const RegisterUser = TryCatch(async (req, res) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
-        return res.status(400).json({
-            message: "All fields (name, email, password) are required",
-        });
+        return res.status(400).json({ message: "All fields are required" });
     }
 
     const UserExist = await User.findOne({ email });
 
     if (UserExist) {
-        return res.status(400).json({
-            message: "Already have an account with this Email",
-        });
+        return res.status(400).json({ message: "Email already registered" });
     }
 
-    try {
-        const HashPassword = await bcrypt.hash(password, 10);
+    const HashPassword = await bcrypt.hash(password, 10);
 
-        const NewUser = await User.create({
-            name,
-            email,
-            password: HashPassword,
-        });
+    const NewUser = await User.create({
+        name,
+        email,
+        password: HashPassword,
+        followers: [],
+        following: [],
+    });
 
-        GenerateToken(NewUser._id, res);
+    await GenerateToken(NewUser._id, res);
 
-        res.status(201).json({
-            user: NewUser,
-            message: "User Registered Successfully",
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message: "Internal Server Error",
-        });
-    }
+    res.status(201).json({ user: NewUser, message: "User Registered Successfully" });
 });
 
+export const LoginUser = TryCatch(async (req, res) => {
+    console.log("Login Request Body:", req.body);
 
-export const LoginUser = TryCatch(async(req, res)=>{
-    
-    const {email, password} = req.body;
-    const user = await User.findOne({email})
+    const { email, password } = req.body;
 
-    if(!user){
-        return res.status(400).json({
-            message: "User Not Registered with this Email, Please Sign Up",
-        });
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(400).json({ message: "User not registered" });
     }
-    const ComparePassword = bcrypt.compare(password, user.password);
-    if(!ComparePassword){
-        return res.status(400).json({
-            message: "Wrong Password, Please Reset it",
-        });
+
+    if (!user.password) {
+        return res.status(500).json({ message: "Password hash missing" });
     }
-    //calling token
 
-    GenerateToken(user._id, res);
+    const ComparePassword = await bcrypt.compare(password, user.password);
+    if (!ComparePassword) {
+        return res.status(400).json({ message: "Wrong password" });
+    }
 
-    res.json({
-        user,
-        message: "Logged in",
-    })
+    await GenerateToken(user._id, res);
 
+    res.status(200).json({ user, message: "Logged in successfully" });
 });
 
-export const MyProfile = TryCatch (async (req, res)=>{
-    const user = await User.findById(req.user._id);
+export const MyProfile = TryCatch(async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
     res.json(user);
 });
 
-export const UserProfile = TryCatch (async (req, res)=>{
+export const UserProfile = TryCatch(async (req, res) => {
     const user = await User.findById(req.params.id).select("-password");
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
     res.json(user);
 });
 
-//follow and unfollow
-export const FollowAndUnFollw = TryCatch(async(req, res)=>{
+export const FollowAndUnFollw = TryCatch(async (req, res) => {
     const user = await User.findById(req.params.id);
     const LoggedInUser = await User.findById(req.user._id);
 
-    if(!user){
-        return res.status(400).json({
-            message: "No user found with this id",
-
-        });
-    }
-    if(user._id.toString() === LoggedInUser._id.toString()){
-        return res.status(400).json({
-            message: "You can't follow yourself",
-        });
-
+    if (!user) {
+        return res.status(400).json({ message: "User not found" });
     }
 
-    if(user.follower.includes(LoggedInUser._id)){
-        const indexFollowing = LoggedInUser.following.indexOf(user._id);
-        const indexFollowers = user.follower.indexOf(LoggedInUser._id);
+    if (user._id.toString() === LoggedInUser._id.toString()) {
+        return res.status(400).json({ message: "You can't follow yourself" });
+    }
 
-        LoggedInUser.following.splice(indexFollowing, 1);
-        user.follower.splice(indexFollowers, 1);
+    user.followers = user.followers || [];
+    LoggedInUser.following = LoggedInUser.following || [];
 
-        await LoggedInUser.save()
-        await user.save()
-        res.json({
-            message: "User Unfollowed",
-        });
-    } else{
+    if (user.followers.includes(LoggedInUser._id)) {
+        user.followers = user.followers.filter((id) => id.toString() !== LoggedInUser._id.toString());
+        LoggedInUser.following = LoggedInUser.following.filter((id) => id.toString() !== user._id.toString());
+
+        await LoggedInUser.save();
+        await user.save();
+        return res.json({ message: "User Unfollowed" });
+    } else {
+        user.followers.push(LoggedInUser._id);
         LoggedInUser.following.push(user._id);
-        user.follower.push(LoggedInUser._id);
 
-        await LoggedInUser.save()
-        await user.save()
-        res.json({
-            message: "User followed",
-        });
+        await LoggedInUser.save();
+        await user.save();
+        return res.json({ message: "User Followed" });
     }
-    
 });
 
-export const LogOut = TryCatch(async(req, res)=>{
-    res.cookie("token" , "", {maxAge: 0});
-
-    res.json({
-        message: "Logged out Succesfully",
-    })
-})
+export const LogOut = TryCatch(async (req, res) => {
+    res.cookie("token", "", { maxAge: 0 });
+    res.json({ message: "Logged out successfully" });
+});
