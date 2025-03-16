673aa0cbf658d72f4bca69ea3c6be2e5b44f7c8b
[Waseem] <[waseemkhan3703@gmail.com]>
2025-03-16 23:39:53 +0500

    created home page

---
+++ frontend/src/context/PinContext.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react"
+
+
+
+    const PinContext = createContext();
+
+    export const PinProvider = ({children}) => {
+      
+        const [pins, setPins] = useState([]);
+        const [loading, setLoading] = useState(true);
+
+
+        async function fetchPins() {
+            try {
+                const {data} = await axios.get("/api/pin/all");
+                setPins(data);
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+
+            }
+        }
+        
+        useEffect(()=>{
+            fetchPins();
+        },[]);
+
+        return (
+        <PinContext.Provider value={{pins, loading}}>{children}</PinContext.Provider>
+        );
+    };
+
+    export const PinData = () => useContext(PinContext);
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,41 @@
-function Home (){
+import { PinData } from "../context/PinContext";
+import { Loading } from "../components/Loading";
 
-    return(
- 
-        <>
-        <h1>Home</h1>
-        </>
 
-    )
-};
+
+
+
+    const Home = () =>{
+
+
+        const {pins, loading} = PinData();
+        console.log(pins)
+
+        return(
+       <div>
+        {loading ? ( <Loading/>
+        ) :(
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <div className="px-4 py-6 sm:px-0">
+                <div className="flex flex-wrap m-4">
+                    {
+                        pins && pins.length > 0 ?(
+                            pins.map((e,i)=> <p key={i}>{e.title}</p>)
+                        ):(
+                            <p>No Pin Yet</p>
+                        )
+                    }
+                </div>
+
+            </div>
+        </div>
+
+        )
+
+        
+    }
+       </div>
+
+        )
+    }
 export default Home;
