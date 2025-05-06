0f7c79210cd3e8f296bb96210ccf537c0f806537
[Waseem] <[waseemkhan3703@gmail.com]>
2025-05-06 23:50:58 +0500

    fix issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './',  // ✅ Add this line to fix path issues
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:5000",
-        changeOrigin: true,         // ✅ Needed for cross-origin
-        secure: false,              // ✅ Allow self-signed certs if needed
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
